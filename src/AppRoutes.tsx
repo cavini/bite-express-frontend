b9af68e7b589a202d2ce5cc94ce99c8a1e59a958
cavini <arthurcavini@hotmail.com>
2024-08-17 16:08:49 -0300

    feat: responsive homepage

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import Layout from "./layouts/layout"
+import HomePage from "./pages/HomePage"
+import Layout from "./layouts/Layout"
 
 const AppRoutes = () => {
   return (
     <Routes>
       
-      <Route path="/" element={<Layout><p>Home Page</p></Layout>} />
+      <Route path="/" element={<Layout><HomePage /></Layout>} />
       <Route path="/user-profile" element={<span>User profile page</span>} />
       <Route path="*" element={<Navigate to='/' />} />
       
