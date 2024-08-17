5034e84806d0a8c02464f1a2299f44930215bffe
cavini <arthurcavini@hotmail.com>
2024-08-17 15:18:45 -0300

    feat: basic layout setup and config

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/layout"
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<span>Home page</span>} />
+      
+      <Route path="/" element={<Layout><p>Home Page</p></Layout>} />
       <Route path="/user-profile" element={<span>User profile page</span>} />
       <Route path="*" element={<Navigate to='/' />} />
-
+      
 
     </Routes>
   )
