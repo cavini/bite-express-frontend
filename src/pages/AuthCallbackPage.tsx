b5e3013e2b9c69855f74cd39f93297ff3c80b37e
cavini <arthurcavini@hotmail.com>
2024-08-19 16:10:48 -0300

    feat: added logic to update user and form

--- src/pages/AuthCallbackPage.tsx
+++ src/pages/AuthCallbackPage.tsx
@@ -1,12 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useCreateMyUser } from "../api/MyUserApi";
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCreateMyUser } from "../api/MyUserApi";
 
 const AuthCallbackPage = () => {
   const navigate = useNavigate();
   const { user } = useAuth0();
   const { createUser } = useCreateMyUser();
+
   const hasCreatedUser = useRef(false);
 
   useEffect(() => {
