b53400638b6f2202a5508cd4f1d83688d8c21716
cavini <arthurcavini@hotmail.com>
2024-08-24 12:48:33 -0300

    chore: minor fixes and integration with react query

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   searchQuery: z.string({
@@ -18,14 +19,19 @@ type Props = {
   onSubmit: (formData: SearchForm) => void;
   placeholder: string;
   onReset?: () => void;
+  searchQuery: string;
 };
-const SearchBar = ({ onSubmit, onReset, placeholder }: Props) => {
+const SearchBar = ({ onSubmit, onReset, placeholder, searchQuery }: Props) => {
   const form = useForm<SearchForm>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      searchQuery: undefined,
+      searchQuery,
     },
   });
+
+  useEffect(() => {
+    form.reset({ searchQuery });
+  }, [form, searchQuery]);
 
   const handleReset = () => {
     form.reset({
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,23 +2,53 @@ import { useParams } from "react-router-dom";
 import { useSearchRestaurants } from "../api/RestaurantApi";
 import { SearchResultInfo } from "../components/SearchResultInfo";
 import SearchResultsCard from "../components/SearchResultsCard";
+import { useState } from "react";
+import SearchBar, { SearchForm } from "../components/SearchBar";
+
+export type SearchState = {
+  searchQuery: string;
+};
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { results, isLoading } = useSearchRestaurants(city);
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+  });
 
-  if (!results?.data || !city) {
-    return <span>No results found</span>;
-  }
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((previousState) => ({
+      ...previousState,
+      searchQuery: searchFormData.searchQuery,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((previousState) => ({
+      ...previousState,
+      searchQuery: "",
+    }));
+  };
 
   if (isLoading) {
     <span>Loading...</span>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>;
   }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
       <div id="cuisines-list">insert cuisines here </div>
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          placeholder="Search by Cuisine or Restaurant Name"
+          onSubmit={setSearchQuery}
+          onReset={resetSearch}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((restaurant) => (
           <SearchResultsCard restaurant={restaurant} />
