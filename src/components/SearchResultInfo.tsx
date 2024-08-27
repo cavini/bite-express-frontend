a0039100bdbfa44befd5de7742d1b948a10c2698
cavini <arthurcavini@hotmail.com>
2024-08-27 19:19:11 -0300

    chore: improve UX

--- src/components/SearchResultInfo.tsx
+++ src/components/SearchResultInfo.tsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom";
-import { capitalizeFirstLetter } from "../lib/utils";
 
 type Props = {
   total: number;
-  city: string;
 };
 
-export const SearchResultInfo = ({ total, city }: Props) => {
+export const SearchResultInfo = ({ total }: Props) => {
   return (
     <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
       <span>
-        {total} Restaurants found in {capitalizeFirstLetter(city)}
+        {total} restaurant{total > 1 ? "s" : ""} found matching your search
         <Link
           className="text-orange-500 text-sm font-semibold hover:underline cursor-pointer ml-1"
           to="/"
